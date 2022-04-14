7df45e599b68f3cfb05c82967552f15f023c079d
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-04-13 20:25:03 -0700

    initialize project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import '../styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,33 @@
-import React from "react"
-import { GetStaticProps } from "next"
-import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
+import React from 'react';
+import { GetStaticProps } from 'next';
+import Layout from '../components/Layout';
+import Post, { PostProps } from '../components/Post';
 
 export const getStaticProps: GetStaticProps = async () => {
   const feed = [
     {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+      id: '1',
+      title: 'Prisma is the perfect ORM for Next.js',
+      content:
+        '[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!',
       published: false,
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        name: 'Nikolas Burk',
+        email: 'burk@prisma.io',
       },
     },
-  ]
-  return { props: { feed } }
-}
+  ];
+  return { props: { feed } };
+};
 
 type Props = {
-  feed: PostProps[]
-}
+  feed: PostProps[];
+};
 
 const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
@@ -51,7 +53,7 @@ const Blog: React.FC<Props> = (props) => {
         }
       `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
