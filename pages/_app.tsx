f95d5d33127c08f7102ec0064d959b9fffed765c
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-04-16 00:57:49 -0700

    added next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import '../styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default App;
