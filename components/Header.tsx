c1d42406d1faf02d9b35e00647c6d3c789c9989a
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-04-25 19:54:07 -0700

    typeform

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { typeform } from '../pages/data';
 
 const Header: React.FC = () => {
   const router = useRouter();
@@ -15,7 +16,7 @@ const Header: React.FC = () => {
           <Image src="/logo.png" width={36} height={48} />
         </a>
       </Link>
-      <Link href="https://google.com">
+      <Link href={typeform}>
         <a
           className="bold text-slate-400 text-sm"
           target="_blank"
