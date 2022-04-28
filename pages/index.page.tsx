4fc4d85ac6c087f99deef43755f07142e6b647da
mding5692 <michael.ding@tophatmonocle.com>
2022-04-28 15:38:19 -0400

    wip: rearrange styles

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -8,45 +8,47 @@ import Filter from '../components/Filter'
 import Teaser, { TeaserProps } from '../components/Teaser';
 
 export const getStaticProps: GetStaticProps = async () => {
-  const companies = await prisma.company.findMany({
-    where: { published: true },
-    include: { links: true, tags: true },
-  });
-  return { props: { companies } };
+    const companies = await prisma.company.findMany({
+        where: { published: true },
+        include: { links: true, tags: true },
+    });
+    return { props: { companies } };
 };
 
 type Props = {
-  companies: TeaserProps[];
+    companies: TeaserProps[];
 };
 
 const Home: React.FC<Props> = ({ companies }) => {
-  const [results, setResults] = useState(companies);
+    const [results, setResults] = useState(companies);
 
-  const handleSearch = (e) => {
-    const query = e.target.value;
-    const searchedCompanies = Object.values(companies).filter((val) =>
-      val.name.toLowerCase().startsWith(query.toLowerCase())
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        const searchedCompanies = Object.values(companies).filter((val) =>
+            val.name.toLowerCase().startsWith(query.toLowerCase())
+        );
+        setResults(searchedCompanies);
+    };
+
+    return (
+        <Layout>
+            <div className="flex flex-col md:flex-row items-center justify-between mb-8">
+                <h2 className="md:max-w-[50%] lg:max-w-[40%] font-black pb-3">
+                    Discover and learn about web3 companies
+                </h2>
+                <Search handleSearch={handleSearch} />
+            </div>
+            <div>
+                <Filter />
+                <div className="w-fit mx-auto md:float-right md:grid grid-cols-2 gap-5">
+                    {results &&
+                        results.map((company) => (
+                            <Teaser key={company.id} company={company} />
+                        ))}
+                </div>
+            </div>
+        </Layout>
     );
-    setResults(searchedCompanies);
-  };
-
-  return (
-    <Layout>
-      <div className="flex flex-col md:flex-row items-center justify-between mb-8">
-        <h2 className="md:max-w-[50%] lg:max-w-[40%] font-black pb-3">
-          Discover and learn about web3 companies
-        </h2>
-        <Search handleSearch={handleSearch} />
-      </div>
-      <div className="w-fit mx-auto md:float-right md:grid grid-cols-2 gap-5">
-        <Filter />
-        {results &&
-          results.map((company) => (
-            <Teaser key={company.id} company={company} />
-          ))}
-      </div>
-    </Layout>
-  );
 };
 
 export default Home;
