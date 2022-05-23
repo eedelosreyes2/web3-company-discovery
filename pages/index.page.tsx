bde61c235be89173ecb7304b8144381ed2df1558
mding5692 <michael.ding@tophatmonocle.com>
2022-05-23 17:55:07 -0400

    test

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -54,6 +54,7 @@ const Home: React.FC<{ companies: CompanyProps[] }> = ({ companies }) => {
     const [menu, setMenu] = useState(false);
 
     useEffect(() => {
+        console.log('...')
         // hotjar initialization
         hotjar.initialize(parseInt(process.env.NEXT_PUBLIC_HOTJAR_ID), parseInt(process.env.NEXT_PUBLIC_HOTJAR_SV))
 
